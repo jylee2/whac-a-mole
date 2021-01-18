8aee994ab7ab8faa6cbadd0aa7608c097c086282
jylee2 <leej1025y@live.com>
2021-01-18 17:58:05 +0800

    Loop to mount Mole Components

--- public/moles.js
+++ public/moles.js
@@ -1,114 +1,56 @@
-// Upon clicking on mole, increment score + unmount mole
-const btnClick = (stateScore, setScore, setMount) => {
-  setScore(stateScore + 1);
-  setMount(false);
+// Mole Button
+const MoleBtn = ({ stateScore, setScore, mountMole, setMountMole }) => {
+  // Upon clicking on mole, increment score + unmount mole
+  const btnClick = (stateScore, setScore, setMountMole) => {
+    setScore(stateScore + 1);
+    setMountMole(false);
+  };
+  return (
+    <div>
+      <button className="classMole" onClick={() => btnClick(stateScore, setScore, setMountMole)}>
+        <div className="classMoleLeftEye"></div>
+        <div className="classMoleRightEye"></div>
+        <div className="classMoleNose"></div>
+      </button>
+    </div>
+  );
 };
 
-// Mole Components
-const MoleOne = ( {stateScore, setScore, setMount} ) => {
+// Mole Hole
+const MoleHole = ( { stateScore, setScore } ) => {
+  const [mountMole, setMountMole] = useState(false);
+  // Make the moles appear/hide at random time intervals
+  const hideShowMole = ( setter ) => {
+    const mainInterval = Math.max(1500, Math.random() * 5000);
+    setInterval( () => {
+      const subInterval = Math.max(1500, Math.random() * 15000);
+      // Use setTimeout, if setInterval then will get faster
+      setTimeout( () => {
+        setter( (state) => {
+          // if true -> false, if false -> true
+          return !state;
+        });
+      }, subInterval );
+    }, mainInterval );
+  };
+  // Similar to componentDidMount, componentDidUpdate, componentWillUnmount
+  useEffect( () => {
+    hideShowMole(setMountMole);
+  }, []);
   return (
-    <div>
-      <button className="classMole" onClick={() => btnClick(stateScore, setScore, setMount)}>
-        <div className="classMoleLeftEye"></div>
-        <div className="classMoleRightEye"></div>
-        <div className="classMoleNose"></div>
-      </button>
-    </div>
-  );
-};
-// Mole Component
-const MoleTwo = ( {stateScore, setScore, setMount} ) => {
-  return (
-    <div>
-      <button className="classMole" onClick={() => btnClick(stateScore, setScore, setMount)}>
-        <div className="classMoleLeftEye"></div>
-        <div className="classMoleRightEye"></div>
-        <div className="classMoleNose"></div>
-      </button>
-    </div>
-  );
-};
-// Mole Component
-const MoleThree = ( {stateScore, setScore, setMount} ) => {
-  return (
-    <div>
-      <button className="classMole" onClick={() => btnClick(stateScore, setScore, setMount)}>
-        <div className="classMoleLeftEye"></div>
-        <div className="classMoleRightEye"></div>
-        <div className="classMoleNose"></div>
-      </button>
-    </div>
-  );
-};
-// Mole Component
-const MoleFour = ( {stateScore, setScore, setMount} ) => {
-  return (
-    <div>
-      <button className="classMole" onClick={() => btnClick(stateScore, setScore, setMount)}>
-        <div className="classMoleLeftEye"></div>
-        <div className="classMoleRightEye"></div>
-        <div className="classMoleNose"></div>
-      </button>
-    </div>
-  );
-};
-// Mole Component
-const MoleFive = ( {stateScore, setScore, setMount} ) => {
-  return (
-    <div>
-      <button className="classMole" onClick={() => btnClick(stateScore, setScore, setMount)}>
-        <div className="classMoleLeftEye"></div>
-        <div className="classMoleRightEye"></div>
-        <div className="classMoleNose"></div>
-      </button>
-    </div>
-  );
-};
-// Mole Component
-const MoleSix = ( {stateScore, setScore, setMount} ) => {
-  return (
-    <div>
-      <button className="classMole" onClick={() => btnClick(stateScore, setScore, setMount)}>
-        <div className="classMoleLeftEye"></div>
-        <div className="classMoleRightEye"></div>
-        <div className="classMoleNose"></div>
-      </button>
-    </div>
-  );
-};
-// Mole Component
-const MoleSeven = ( {stateScore, setScore, setMount} ) => {
-  return (
-    <div>
-      <button className="classMole" onClick={() => btnClick(stateScore, setScore, setMount)}>
-        <div className="classMoleLeftEye"></div>
-        <div className="classMoleRightEye"></div>
-        <div className="classMoleNose"></div>
-      </button>
-    </div>
-  );
-};
-// Mole Component
-const MoleEight = ( {stateScore, setScore, setMount} ) => {
-  return (
-    <div>
-      <button className="classMole" onClick={() => btnClick(stateScore, setScore, setMount)}>
-        <div className="classMoleLeftEye"></div>
-        <div className="classMoleRightEye"></div>
-        <div className="classMoleNose"></div>
-      </button>
-    </div>
-  );
-};
-// Mole Component
-const MoleNine = ( {stateScore, setScore, setMount} ) => {
-  return (
-    <div>
-      <button className="classMole" onClick={() => btnClick(stateScore, setScore, setMount)}>
-        <div className="classMoleLeftEye"></div>
-        <div className="classMoleRightEye"></div>
-        <div className="classMoleNose"></div>
-      </button>
+    <div className="classMoleHole">
+      {/* if mount = true, then mount <Component/> */}
+      { (mountMole === true)
+        ?
+        <MoleBtn
+          stateScore = {stateScore}
+          setScore = {setScore}
+          mountMole = {mountMole}
+          setMountMole = {setMountMole}
+        />
+        :
+        null
+      }
     </div>
   );
 };
